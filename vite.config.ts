1396e913967f78661fccb86b8edaa6ae623c844f
yitingxiao23-cpu <yitingxiao23@gmail.com>
2026-07-10 23:34:49 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/table-tennis-book/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
